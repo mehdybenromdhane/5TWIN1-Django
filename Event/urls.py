77ba80db9744cd79414d51538e491f319b0b5a56
mehdybenromdhane <mehdybenromdhane9@gmail.com>
2025-09-26 15:15:58 +0100

    Read and details event

--- Event/urls.py
+++ Event/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 
-from .views import hello
+from .views import *
 urlpatterns = [
     
-    path('', hello )
+    path('', hello ),
+    
+    path('list/' , listEvents , name="listfunction"),
+    
+    path('listClass/' , EventList.as_view() , name="listClass"),
+    
+    path('details/<int:ide>', details , name="details"),
+    
+    path('detailsClass/<str:pk>',  DetailsEvent.as_view()    , name="detailsClass")
 ]
--- Event/views.py
+++ Event/views.py
@@ -1,11 +1,48 @@
 from django.shortcuts import render
 
 # Create your views here.
-
-
+from .models import Event
+from django.views.generic import ListView, DeleteView,DetailView,CreateView,UpdateView
 
 def hello(request):
     
     classe= "5 TWIN1"
     
     return render(request, "event/bonjour.html" , {"c1":classe})
+
+
+
+def listEvents(request):
+    
+    
+    events = Event.objects.filter(state=True)
+    
+    
+    return render(request,'event/list.html' , {"list":events})
+
+
+
+class EventList(ListView):
+    
+    model =Event
+    
+    template_name='event/list.html'
+    
+    context_object_name="list"
+    
+    
+
+def details(request, ide):
+    
+    event =Event.objects.get(id=ide)
+    
+    
+    return render(request, 'event/details.html' , {'event':event})
+
+
+
+class DetailsEvent(DetailView):
+    
+    model=Event
+    template_name="event/details.html"
+    context_object_name="event"
